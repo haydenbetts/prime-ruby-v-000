bafac42306e0755f119df5fdf3cbe60539d7b761
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 17:23:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,5 @@
-require 'pry'
-
 # using trial division
 def prime?(num)
-  # generate an array of nums from 2 to sqrt n
   return false if num.abs == 1 || num == 0
 
   trial_divisors = (2..num - 1).to_a
