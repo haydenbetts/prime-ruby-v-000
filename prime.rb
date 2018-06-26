1b1510a0390ace2383a505e620185ca0db11fb92
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 17:20:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,9 @@ def prime?(num)
   return false if num.abs == 1 || num == 0
 
   trial_divisors = (2..num).to_a
-  trial_divisors.collect do |divisor|
+  division_results = trial_divisors.collect do |divisor|
     !(num.abs % divisor == 0)
   end
+
+  division_results.detect?(true)
 end
