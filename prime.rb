de64e889d865c1952ba6a7c13926946c28ee1594
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 17:12:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ require 'pry'
 def prime?(num)
   # generate an array of nums from 2 to sqrt n
   trial_divisors = (2..num).to_a
-  trial_divisors.none? do |divisor|
-    num.abs % divisor == 0
+  trial_divisors.include? do |divisor|
+    !(num.abs % divisor == 0)
   end
 end
