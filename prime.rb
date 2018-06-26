c70735eb2486530b1648127952a427b15975b50a
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 17:14:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ require 'pry'
 # using trial division
 def prime?(num)
   # generate an array of nums from 2 to sqrt n
-  return false if num.abs == 1
+  return false if num.abs == 1 || num == 0
 
   trial_divisors = (2..num).to_a
   trial_divisors.each do |divisor|
